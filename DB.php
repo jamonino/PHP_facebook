222907ba1654362579df19afa355be0e48cd8210
josem <josem@LAPTOP-S1P10O6I>
2022-02-21 10:17:49 +0100

    initsdff

--- DB.php
+++ DB.php
@@ -1,14 +1,12 @@
 <?php
 
-include 'Parameters.php';
-
 class DB {
     public $connection;
     public function __construct() {
         $db_host        = "host = 127.0.0.1";
         $db_port        = "port = 5432";
-        $db_name      = "dbname = twitter_db";
-        $db_credentials = "user = twitter_user password=12345";
+        $db_name      = "dbname = facebook_db";
+        $db_credentials = "user = facebook_user password=12345";
         $this->connection = pg_connect("$db_host $db_port $db_name $db_credentials");
     }
 
